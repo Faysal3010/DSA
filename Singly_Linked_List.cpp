bc1cba5b6d66c3cee1c3fd96d7c583719de88d84
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-01-09 17:12:35 +0600

    Create Singly_Linked_List.cpp

---
+++ Singly_Linked_List.cpp
@@ -0,0 +1,31 @@
+//Singly Linked List-এ প্রতিটি নোডের মধ্যে একটি ডাটা এবং পরবর্তী নোডের রেফারেন্স থাকে। এটি একমুখী।
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+}
+
+int main()
+{
+  //new memory dynamic kore memory address return kore
+  // -> access korar jonne use kora hoi
+    Node *head = new Node{1, NULL};
+    head->next = new Node{5, NULL};
+    head->next->next = new Node{9, NULL};
+
+    print(head);
+
+    return 0;
+}
